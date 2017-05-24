4fca48a0367e83d6a3190e891b7901b618a5e661
Mark Eschbach <mark@rhumbix.com>
2017-05-24 11:42:50 -0700

    Moves test port outside of privileged range

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ var Syslogd = require('../')
 
 var time = 'Dec 15 10:58:44'
 var testMsg = '<183>' + time + ' hostname tag: info'
+const port = 10514
 
 Syslogd(function(info) {
     //console.log(info)
@@ -23,14 +24,14 @@ Syslogd(function(info) {
     assert.deepEqual(shouldRet, info)
     console.log('test pass!')
     process.exit(0)
-}).listen(514, function(err) { // sudo
+}).listen( port, function(err) { // sudo
     console.log('listen', err)
     assert(!err)
     var client = dgram.createSocket('udp4')
     var buffer = new Buffer(testMsg)
-    client.send(buffer, 0, buffer.length, 514, 'localhost', function(err, bytes) {
+    client.send(buffer, 0, buffer.length, port, 'localhost', function(err, bytes) {
         console.log('send', err, bytes)
-    })       
+    })
 })
 
 
